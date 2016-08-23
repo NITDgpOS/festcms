2d9092eafbd8ed479fe6cb159b197994f0264944
Bishakh Ghosh <ghoshbishakh@gmail.com>
2016-08-23 22:44:05 +0530

    Add a profile model associated with user

--- festflow/models.py
+++ festflow/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    "Stores additional information about the user"
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
