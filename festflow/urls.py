d5c2ee9df7d79dc3f871947538efff1c34a88ff6
Bishakh Ghosh <ghoshbishakh@gmail.com>
2016-08-24 14:52:55 +0530

    Add basic social authentication

--- festflow/urls.py
+++ festflow/urls.py
@@ -1,7 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib.auth.views import logout
 from . import views
 
 urlpatterns = [
     # Home Page
     url(r'^$', views.index, name='index'),
+
+    # social login urls
+    url('', include('social.apps.django_app.urls', namespace='social')),
+
+    # logout url
+    url(r'^logout/$', logout,
+        {'next_page': '/'})
 ]
