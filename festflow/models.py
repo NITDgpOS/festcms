bd0fdd8457828bdd0610f7ed88c86c1a0e3d6a0f
Bishakh Ghosh <ghoshbishakh@gmail.com>
2016-08-27 16:08:41 +0530

    Add event model and add many to many field from user to event

--- festflow/models.py
+++ festflow/models.py
@@ -4,6 +4,33 @@ from django.db import models
 # Create your models here.
 
 
+class Event(models.Model):
+    "Stores information about the events in the fest"
+
+    # event name
+    name = models.CharField(max_length=255)
+
+    # event description
+    description = models.TextField()
+
+    # event problem statement file
+    problem_statement_file = models.FileField(upload_to='problem_statements/',
+                                              blank=True, null=True)
+
+    # event venue
+    venue = models.CharField(max_length=255)
+    venue_lat = models.DecimalField(max_digits=9, decimal_places=8,
+                                    blank=True, null=True)
+    venue_lon = models.DecimalField(max_digits=9, decimal_places=8,
+                                    blank=True, null=True)
+
+    # event date and time
+    date_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
     "Stores additional information about the user"
 
@@ -16,6 +43,9 @@ class Profile(models.Model):
     major_subject_name = models.CharField(max_length=255)
 
     phone_number = models.CharField(max_length=15)
+
+    # events registered for
+    registered_events = models.ManyToManyField(Event)
 
     def __str__(self):
         return self.user.get_full_name()
