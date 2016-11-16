2a98638bdcf4469828da2948f87204c2c77b98ae
oindreebasu <oindreebasu97@gmail.com>
2016-11-16 23:27:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='YourAppName',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='Bishakh Ghosh',
+      author_email='ghoshbishakh@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
