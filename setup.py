cfb04b2fe943e77d1154147dd195361979a0eae1
Tejesh <tejesh.silarapu@gmail.com>
2016-11-17 12:17:10 +0530

    Resolved #36: Changed description in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='YourAppName',
       version='1.0',
-      description='OpenShift App',
+      description='A generic CMS for college fests.',
       author='Bishakh Ghosh',
       author_email='ghoshbishakh@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
