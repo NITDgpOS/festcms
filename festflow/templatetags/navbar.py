2f924f125c718242163adf42efca216e188a9b1b
Bishakh Ghosh <ghoshbishakh@gmail.com>
2017-01-31 15:04:50 +0530

    Fix PEP8 errors

--- festflow/templatetags/navbar.py
+++ festflow/templatetags/navbar.py
@@ -1,9 +1,9 @@
 from django import template
-from django.core.urlresolvers import resolve, Resolver404
 
 from festflow.models import NavbarEntry
 
 register = template.Library()
+
 
 @register.inclusion_tag('festflow/navbar.html', takes_context=True)
 def navbar(context):
